6b5d20ee4cf742e313e9a0c9a8400706ed2fc8b1
Aarsyth <39145409+tobyarnett@users.noreply.github.com>
2023-05-29 14:21:18 -0500

    Update setup_config.py

--- scripts/setup_config.py
+++ scripts/setup_config.py
@@ -30,25 +30,45 @@
 
 import os
 
-def create_config():
-    torrc_config = """
-    ## Configuration file for a typical Tor user
-    ## Last updated 9 October 2010 for Tor 0.2.2.1-alpha.
-    ## (may or may not work for much older or much newer versions of Tor.)
-    
-    ########## Tor opens a SOCKS proxy on port 9050 by default -- even if you don't
-    ## configure one below. Set "SOCKSPort 0" if you plan to run Tor only
-    ## as a relay, and not make any local application connections yourself.
-    #SOCKSPort 9050 # Default: Bind to localhost:9050 for local connections.
-    #SOCKSPort 192.168.0.1:9100 # Bind to this address:port too.
+def create_hidden_service_directory():
+    hidden_service_path = '/var/lib/torbrowser/config/hidden_service/'
+    os.makedirs(hidden_service_path, exist_ok=True)
+    os.chmod(hidden_service_path, 0o700)
 
-    """
+def get_bridge_info():
+    bridge_address = input("Enter the bridge address: ")
+    bridge_fingerprint = input("Enter the bridge fingerprint: ")
+    return bridge_address, bridge_fingerprint
+
+def create_config(bridge_address, bridge_fingerprint):
+    torrc_config = f"""
+# This is the torrc file that will be written by the setup_config.py script.
+# It contains configuration options for the Tor service.
+# The options written here will be overwritten by the script when it runs.
+
+# SOCKS proxy configuration
+SOCKSPort 9050
+SOCKSPolicy accept 127.0.0.1
+SOCKSPolicy accept ::1
+
+# Enable directory authorities for bootstrap
+UseBridges 1
+Bridge obfs4 {bridge_address} {bridge_fingerprint}
+
+# Hidden service configuration
+HiddenServiceDir /var/lib/torbrowser/config/hidden_service/
+HiddenServicePort 80 127.0.0.1:8080
+"""
+
     with open('../config/torrc', 'w') as file:
         file.write(torrc_config)
 
 def main():
+    print("Tor Configuration Setup")
+    create_hidden_service_directory()
+    bridge_address, bridge_fingerprint = get_bridge_info()
     print("Creating torrc configuration...")
-    create_config()
+    create_config(bridge_address, bridge_fingerprint)
     print("Configuration setup is complete!")
 
 if __name__ == "__main__":
